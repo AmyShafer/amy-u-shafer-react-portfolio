b3ec0f43a60e1522dfbc50b749310ff6e28b76cc
Amy Shafer <amy.u.shafer@gmail.com>
2022-07-18 18:50:32 -0400

    building out components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import '../styles/Header.css';
+
+function Header(props) {
+  return ( <header className = "header" >
+    <h1 > Home < /h1> <
+    /header>
+  );
+}
+
+export default Header;
