f9ec6a94dad4eb02eb66e07b5b4f92ce08638375
Amy Shafer <amy.u.shafer@gmail.com>
2022-07-20 14:12:11 -0400

    file reorganization

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return ( < footer className = "footer" >
+    <
+    h1 > Footer < /h1> <
+    p > Amy Shaferⓒ 2022 < /p> < /
+    footer >
+  );
+}
+
+export default Footer;
