245a9e4e3260b7fe8c2c1c044e1e69b9032d4b3f
Amy Shafer <amy.u.shafer@gmail.com>
2022-07-20 21:33:52 -0400

    progress on the project file bug

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,25 +5,25 @@ import Project from '../Project';
 
 function Portfolio() {
   const [projects] = useState([{
-      name: "CS Icons Quiz",
+      name: "cs-quiz",
       description: "Take this quiz to learn about computer science icons. Select the correct answer before the timer runs out. See if you can make the scoreboard!",
       link: "https://amyshafer.github.io/CS-Icons-Quiz/",
       repo: "https://github.com/AmyShafer/CS-Icons-Quiz"
     },
     {
-      name: "It's Raining Again!",
+      name: "weather-girls-app",
       description: "We're your Weather Girls and have we got news for you! You betta listen -- We have a new weather app to put all your weather worries to rest. We might not be able to help with your love life (this time) but until that miracle blesses our skies again, we have just the thing for all your climate quandaries.",
       link: "https://amyshafer.github.io/Its-Raining-Again-Or-Is-It/",
       repo: "https://github.com/AmyShafer/Its-Raining-Again-Or-Is-It"
     },
     {
-      name: "Mad Archer's Cookbook",
+      name: "mad-archers",
       description: "Mad Archer's Cookbook helps a hungry public by allowing users to search for recipes by entering in ingredients they already have on hand.",
       link: "https://amyshafer.github.io/Mad-Archers-Cookbook/",
       repo: "https://github.com/AmyShafer/Mad-Archers-Cookbook"
     },
     {
-      name: "Scheduler for the Wicked",
+      name: "scheduler666",
       description: "Grave digging? Reanimating of lifeless tissue? Coven meetings? Scrolls are so 7th century! Give Modern Paganism a chance with this brand new app for the morally corrupt and immortally upped.",
       link: "https://amyshafer.github.io/Scheduler-for-the-Wicked/",
       repo: "https://github.com/AmyShafer/Scheduler-for-the-Wicked"
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,122 +1,55 @@
 import React from "react";
+import {
+  removeHyphensAndCapitalize
+} from '../../utils/helpers';
 
 
-function Project(props) {
+function Project({
+  project
+}) {
+  const {
+    name,
+    repo,
+    link,
+    description
+  } = project;
+
   return ( <
-      section id = "work"
-      class = "work" >
-      <
-      section id = "white-half3" >
-      <
-      section id = "work-text" >
-      <
-      img id = "work-header"
-      src = "./assets/images/work.png"
-      alt = "work-header" / >
-      <
-      p > Here is my work so far: < /p>
-
-      <
-      div class = "dropdown mt-1" >
-      <
-      button class = "btn btn-warning dropdown-toggle"
-      type = "button"
-      id = "dropdownMenuButton1"
-      data - toggle = "dropdown"
-      aria - expanded = "false" >
-      Featured Projects < /button>
-
-      <
-      ul class = "dropdown-menu hidden"
-      aria - labelledby = "dropdownMenuButton1" >
-      <
-      li >
-      <
-      a class = "dropdown-item"
-      data - set = "0"
-      href = "#" >
-      CS Icons Quiz < /a> <
-      /li> <
-      li >
-      <
-      a class = "dropdown-item"
-      data - set = "1"
-      href = "#" >
-      Weather Girls App < /a> <
-      /li> <
-      li >
-      <
-      a class = "dropdown-item"
-      data - set = "2"
-      href = "#" >
-      Mad Archer 's Cookbook <
-      /a> </li >
-      <
-      li >
-      <
-      a class = "dropdown-item"
-      data - set = "3"
-      href = "#" > Scheduler < /a> <
-      /li> <
-      /ul> <
-      /div>
-
-      <
-      div class = "container" >
-      <
-      div id = "work-square" > {
-        /*  Deployment Link  */
-      } <
-      a href = "#"
-      target = "_blank"
-      data - set = "4" > < /a> <
-      /div> {
-      /*  Month/Year  */
-    } <
-    div id = "work-circle" >
+    div className = "project"
+    key = {
+      name
+    } >
     <
-    a href = "#"
-  target = "_blank"
-  data - set = "5" > < /a> <
-    /div> {
-  /*  Repo Link */
-} <
-div id = "work-triangle" >
-  <
-  a href = "#"
-target = "_blank"
-data - set = "6" >
-  <
-  /a> <
-  /div> <
-  /div> <
-  /section> <
-  /section> <
-  section id = "black-half3" >
-  <
-  div id = "yellow-triangle"
-class = "show" >
-  <
-  /div> <
-  div id = "blue-triangle"
-class = "show" >
-  <
-  /div> <
-  div id = "pink-triangle"
-class = "show" >
-  <
-  /div> <
-  div id = "no-triangles"
-class = "hidden" >
-  <
-  img id = "project-gif"
-src = ""
-alt = "" / >
-  <
-  /div> < /
-  section > <
-  /section>
-);
+    img src = {
+      require(`../../assets/projects/${name}.gif`)
+    }
+    alt = {
+      removeHyphensAndCapitalize(name)
+    }
+    className = "project-bg" /
+    >
+    <
+    div className = "project-text" >
+    <
+    h3 >
+    <
+    a href = {
+      link
+    } > {
+      removeHyphensAndCapitalize(name)
+    } < /a>{' '} <
+    a href = {
+      repo
+    } >
+    <
+    /a> <
+    /h3> <
+    p > {
+      description
+    } < /p> <
+    /div> <
+    /div>
+  );
 }
 
 export default Project;
