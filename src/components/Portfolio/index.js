ad627ee2a63029d46520c48fac8d7b21d0f96410
Amy Shafer <amy.u.shafer@gmail.com>
2022-07-20 22:09:44 -0400

    images are present

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -33,7 +33,7 @@ function Portfolio() {
   return ( <
     div >
     <
-    div className = "flex-row" > {
+    div className = "flex-column" > {
       projects.map((project, idx) => ( <
         Project project = {
           project
